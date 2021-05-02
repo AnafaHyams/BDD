97abb72d1215daa682f661f1f7e03f8c529778a0
AnafaHyams <anafa18@gmail.com>
2021-05-02 19:14:22 +0300

    mini project 2-5-2021: create resources package

---
+++ src/main/java/java_basic_course/day_02_05_2021/mini_project/Resources/QuotesBucket.java
@@ -0,0 +1,14 @@
+package java_basic_course.day_02_05_2021.mini_project.Resources;
+
+import java.util.List;
+
+public class QuotesBucket {
+
+    public static List<String> textSuppliers = List.of(
+            "The future belongs to those who believe in the beauty of their dreams",
+            "The way to get started is to quit talking and begin doing",
+            "Your time is limited, so don't waste it living someone else's life",
+            "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough",
+            "Life is what happens when you're busy making other plans"
+    );
+}
