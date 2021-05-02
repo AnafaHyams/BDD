dfbfcd98333dab10240b8fe7903e35989a74adc9
AnafaHyams <anafa18@gmail.com>
2021-05-02 19:13:32 +0300

    mini project 2-5-2021: create resources package

--- src/main/java/java_basic_course/day_02_05_2021/mini_project/task1/Main.java
+++ src/main/java/java_basic_course/day_02_05_2021/mini_project/task1/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         QuoterProducer quoterProducer = new QuoterProducer();
 
-        Quote quote = quoterProducer.getRandomQuote();
+        Quote quote = quoterProducer.getRandomQuoteObject();
         System.out.println(quote);
         WriteToFolderHandler.writeObject(quote, ConstUtilities.OBJECTS_DIR_PATH, ConstUtilities.END_TXT_FILE_PATH_FORMAT);
     }
--- src/main/java/java_basic_course/day_02_05_2021/mini_project/task1/QuoterProducer.java
+++ src/main/java/java_basic_course/day_02_05_2021/mini_project/task1/QuoterProducer.java
@@ -1,12 +1,11 @@
 package java_basic_course.day_02_05_2021.mini_project.task1;
 
+import java_basic_course.day_02_05_2021.mini_project.Resources.QuotesBucket;
 import java_basic_course.day_02_05_2021.mini_project.Utilities.RandomUtil;
-
-import java.util.List;
 
 public class QuoterProducer {
 
-    public Quote getRandomQuote() {
+    public Quote getRandomQuoteObject() {
         long id = System.nanoTime();
         String text = createRandomText();
 
@@ -19,17 +18,7 @@ public class QuoterProducer {
         return Status.findByDbCode(textLength);
     }
 
-
-    private List<String> textSuppliers = List.of(
-            "The future belongs to those who believe in the beauty of their dreams",
-            "The way to get started is to quit talking and begin doing",
-            "Your time is limited, so don't waste it living someone else's life",
-            "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough",
-            "Life is what happens when you're busy making other plans"
-    );
-
-
     private String createRandomText() {
-        return RandomUtil.getRandomItem(textSuppliers);
+        return RandomUtil.getRandomItem(QuotesBucket.textSuppliers);
     }
 }
